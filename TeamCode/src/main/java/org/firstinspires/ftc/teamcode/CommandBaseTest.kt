5e63aabf01f7168f7b43437e97a5595220e89ad1
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2025-09-09 00:37:20 -0400

    Jitpack Integration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBaseTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBaseTest.kt
@@ -4,10 +4,19 @@ import com.millburnx.cmdx.Command
 import com.millburnx.cmdx.runtimeGroups.CommandScheduler
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import kotlin.math.abs
+import kotlin.math.max
 
 @TeleOp(name = "CommandBaseTest")
 class CommandBaseTest : LinearOpMode() {
     val scheduler = CommandScheduler()
+
+    val fl by lazy { hardwareMap["fl"] as DcMotorEx }
+    val bl by lazy { hardwareMap["bl"] as DcMotorEx }
+    val fr by lazy { hardwareMap["fr"] as DcMotorEx }
+    val br by lazy { hardwareMap["br"] as DcMotorEx }
 
     override fun runOpMode() {
         scheduler.schedule(
@@ -15,5 +24,20 @@ class CommandBaseTest : LinearOpMode() {
                 println("Hello World!")
             },
         )
+        fl.direction = DcMotorSimple.Direction.REVERSE
+        bl.direction = DcMotorSimple.Direction.REVERSE
+        waitForStart()
+        while (opModeIsActive() && !isStopRequested) {
+            val y = -
+            gamepad1.left_stick_y
+            val x = gamepad1.left_stick_x * 1.1
+            val rx = gamepad1.right_stick_x
+
+            val denom = max(abs(y) + abs(x) + abs(rx), 1.0)
+            fl.power = (y + x + rx) / denom
+            bl.power = (y - x + rx) / denom
+            fr.power = (y - x - rx) / denom
+            br.power = (y + x - rx) / denom
+        }
     }
 }
