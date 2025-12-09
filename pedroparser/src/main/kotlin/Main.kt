25a60511b9a4f9523f5deec877692f442fdcf721
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2025-12-09 10:20:56 -0500

    Functions

--- pedroparser/src/main/kotlin/Main.kt
+++ pedroparser/src/main/kotlin/Main.kt
@@ -4,20 +4,21 @@ import com.millburnx.pedroparser.types.RawSequence
 import com.millburnx.pedroparser.types.Sequence
 import kotlinx.serialization.json.Json
 
-public val json: Json = Json {
+public val SequenceJson: Json = Json {
     ignoreUnknownKeys = true
     classDiscriminator = "heading"
 }
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public fun decodeJson(json: String): RawSequence = SequenceJson.decodeFromString<RawSequence>(json)
+
+public fun mapRawData(rawSequence: RawSequence): Sequence = Sequence.fromRaw(rawSequence)
+
+public fun parseSequence(json: String): Sequence = mapRawData(decodeJson(json))
+//
 public fun main() {
     val inputStream = object {}.javaClass.getResourceAsStream("/trajectory.pp")
         ?: throw IllegalArgumentException("Resource not found: trajectory.pp")
 
     val jsonText = inputStream.bufferedReader().use { it.readText() }
-
-    val raw = json.decodeFromString<RawSequence>(jsonText)
-
-    println(Sequence.fromRaw(raw))
+    println(parseSequence(jsonText))
 }
