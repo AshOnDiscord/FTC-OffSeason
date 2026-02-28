a5f61f30b2d5263f465bb510429408afbb5b9ab3
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2026-02-27 19:57:24 -0500

    hopefully fix pedro getting stuck lol

--- cmdxpedro/src/main/kotlin/PathingCommands.kt
+++ cmdxpedro/src/main/kotlin/PathingCommands.kt
@@ -5,12 +5,29 @@ import com.millburnx.cmdxpedro.util.WaitFor
 import com.pedropathing.follower.Follower
 import com.pedropathing.paths.PathChain
 
-public fun FollowPath(follower: Follower, path: PathChain, maxPower:Double, opModeIsActive: () -> Boolean): Command = Command() {
-    follower.followPath(path, maxPower, true)
-    WaitFor { !opModeIsActive() || !(follower.isBusy) }
-}
+public fun FollowPath(
+    follower: Follower,
+    path: PathChain,
+    maxPower: Double,
+    opModeIsActive: () -> Boolean,
+    translationalTolerance: Double = 1.0,
+    headingTolerance: Double = 5.0,
+): Command =
+    Command {
+        follower.followPath(path, maxPower, true)
+        WaitFor {
+            !opModeIsActive() || !follower.isBusy || follower.atParametricEnd() || follower.isRobotStuck ||
+                follower.atPose(path.endPose(), translationalTolerance, headingTolerance) ||
+                follower.atPose(path.endPoint(), translationalTolerance, headingTolerance)
+        }
+    }
 
-public fun TurnTo(follower: Follower, degrees: Double, opModeIsActive: () -> Boolean): Command = Command() {
-    follower.turnToDegrees(degrees)
-    WaitFor { !opModeIsActive() || !(follower.isBusy || follower.isTurning) }
-}
+public fun TurnTo(
+    follower: Follower,
+    degrees: Double,
+    opModeIsActive: () -> Boolean,
+): Command =
+    Command {
+        follower.turnToDegrees(degrees)
+        WaitFor { !opModeIsActive() || !(follower.isBusy || follower.isTurning) }
+    }
